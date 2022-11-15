558439e255b5068bc926719b9a44b4295bfbba29
Phil Marcus <pmarcus@reverek12.org>
2022-11-15 14:24:32 -0500

    formatted TwoButtonsAndALabel

--- Unit 3 Assignements/src/TwoButtonsAndALabel.java
+++ Unit 3 Assignements/src/TwoButtonsAndALabel.java
@@ -11,21 +11,23 @@ import javax.swing.JPanel;
 
 public class TwoButtonsAndALabel extends JFrame implements ActionListener {
 
-	//this class starts with 3 attributes. Two are JButton objects for the user to click 
-	// and one is a JLabel object which displays some text.
-	//Starter code by Marcus
+	// this class starts with 3 attributes. Two are JButton objects for the user to
+	// click and one is a JLabel object which displays some text.
+	// Starter code by Marcus
 	static JLabel label = new JLabel("default text");
 	static JButton left = new JButton("left button");
 	static JButton right = new JButton("right button");
 
 	public static void setup() {
-		//initialize your buttons and label here, each JButton and JLabel object has a setText(String) method you can call.
-		//Also do anything else that needs to be done before the user clicks anything.
+		// initialize your buttons and label here, each JButton and JLabel object has a
+		// setText(String) method you can call.
+		// Also do anything else that needs to be done before the user clicks anything.
 	}
-	
+
 	public void buttonClick(JButton chosen) {
-		//this method is called whenever the user clicks a button. The parameter is a JButton object, either
-		//"left" or "right"
+		// this method is called whenever the user clicks a button. The parameter is a
+		// JButton object, either
+		// "left" or "right"
 	}
 
 	private static void createAndShowGUI() {
@@ -51,10 +53,9 @@ public class TwoButtonsAndALabel extends JFrame implements ActionListener {
 		c.setLayout(new BoxLayout(c, BoxLayout.PAGE_AXIS));
 		c.add(text);
 		c.add(buttons);
-		
-		//run your setup method
-		setup();
 
+		// run your setup method
+		setup();
 
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
@@ -73,9 +74,10 @@ public class TwoButtonsAndALabel extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//when a button is clicked, calls your buttonClick method with whichever JButton object was clicked passed as the argument
+		// when a button is clicked, calls your buttonClick method, passing as
+		// an argument whichever JButton object was clicked
 		buttonClick((JButton) e.getSource());
-		
+
 	}
 
 }
